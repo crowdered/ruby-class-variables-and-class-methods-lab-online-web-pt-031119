dc27db2fccea4ba44c0bf1f9f32208935688bf8d
Mark Crowder <crowdered@gmail.com>
2019-04-03 14:22:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,6 +23,18 @@ def self.artists
   @@artists.uniq
 end
 
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
 def self.genres
   @@genres.uniq
 end
